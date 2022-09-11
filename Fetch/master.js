69bb87a957cfa17a9cc8dfe520e49cacdbbcfd2c
ArashkKH <106018197+ArashkKH@users.noreply.github.com>
2022-09-11 09:13:06 +0430

    added files

---
+++ Fetch/master.js
@@ -0,0 +1,13 @@
+{
+    let i = 1
+    function logDiv() {
+        console.log('------------'+i+'------------')
+        i++
+    }
+}
+
+function getLog(log){
+    console.log(log)
+}
+//----------------------------------------//
+
